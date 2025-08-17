825fa91da320e4f9634b9790241bf3ac18797024
Trieu112233 <22022110@vnu.edu.vn>
2025-08-17 18:02:52 +0700

    complete

--- Game.cpp
+++ Game.cpp
@@ -5,8 +5,7 @@
 #include "TopScoreScreen.h"
 
 Game::Game() : window(sf::VideoMode(980, 800), "Racing Game"), lastScore(0) {
-    currentScreen = std::make_unique<MenuScreen>();
-    
+    currentScreen = std::unique_ptr<MenuScreen>(new MenuScreen());
     if (!mainMenuMusic.openFromFile("menu_theme_music.wav")) {};
     mainMenuMusic.setLoop(true);
     mainMenuMusic.play();
@@ -26,11 +25,11 @@ void Game::run() {
             
             if (dynamic_cast<MenuScreen*>(currentScreen.get())) {
                 if (next == 1) {
-                    currentScreen = std::make_unique<PlayingScreen>(highScoreManager);
+                    currentScreen = std::unique_ptr<PlayingScreen>(new PlayingScreen(highScoreManager));
                     mainMenuMusic.stop();
                 }
                 else if (next == 2) {
-                    currentScreen = std::make_unique<TopScoreScreen>(highScoreManager);
+                    currentScreen = std::unique_ptr<TopScoreScreen>(new TopScoreScreen(highScoreManager));
                 }
                 else if (next == 3) {
                     window.close();
@@ -40,17 +39,17 @@ void Game::run() {
                 if (next == 2) { // Game Over
                     PlayingScreen* ps = static_cast<PlayingScreen*>(currentScreen.get());
                     lastScore = ps->getScore();
-                    currentScreen = std::make_unique<GameOverScreen>(lastScore);
+                    currentScreen = std::unique_ptr<GameOverScreen>(new GameOverScreen(lastScore));
                     mainMenuMusic.play();
                 }
             }
             else if (dynamic_cast<GameOverScreen*>(currentScreen.get())) {
                 if (next == 1) { // Play Again
-                    currentScreen = std::make_unique<PlayingScreen>(highScoreManager);
+                    currentScreen = std::unique_ptr<PlayingScreen>(new PlayingScreen(highScoreManager));
                     mainMenuMusic.stop();
                 }
                 else if (next == 2) { // Main Menu
-                    currentScreen = std::make_unique<MenuScreen>();
+                    currentScreen = std::unique_ptr<MenuScreen>(new MenuScreen());
                 }
                 else if (next == 3) { // Exit
                     window.close();
@@ -58,7 +57,7 @@ void Game::run() {
             }
             else if (dynamic_cast<TopScoreScreen*>(currentScreen.get())) {
                 if (next == 1) { // Back to Menu
-                    currentScreen = std::make_unique<MenuScreen>();
+                    currentScreen = std::unique_ptr<MenuScreen>(new MenuScreen());
                 }
             }
         }
